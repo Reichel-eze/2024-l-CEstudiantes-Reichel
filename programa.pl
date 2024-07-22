66e38556d35c06137b6fa82a150b9b27d83c9320
Reichel-eze <reicheleze@gmail.com>
2024-07-22 16:53:35 -0300

    segunda parte hecha

--- programa.pl
+++ programa.pl
@@ -17,6 +17,7 @@ estudiantes(sistemas, 2018, unter).
 % Cantidad de votos obtenidos en cada eleccion
 % votos(Agrupacion, Voto, Carrera, Anio).
 votos(franjaMorada, 213, sistemas, 2017).
+votos(franjaMorada, 213, civil, 2017).
 votos(franjaMorada, 21323, sistemas, 2019).
 votos(agosto29, 21323, sistemas, 2019).
 
@@ -63,3 +64,51 @@ huboFraude(Anio) :-
 
 % 4) Todos los años en que hubo fraude
 % ?- huboFraude(Anio).
+
+% SEGUNDA parte: Nos llega información extraoficial sobre las acciones que realiza cada agrupación
+
+realizoAccion(franjaNaranja, lucha(salarioDocente)).
+realizoAccion(franjaNaranja, gestionIndividual("Excepción de correlativas", juanPerez, 2019)).
+realizoAccion(franjaNaranja, obra(2019)).
+realizoAccion(agosto29, lucha(salarioDocente)).
+realizoAccion(agosto29, lucha(boletoEstudiantil)).
+
+% Encontrar a las agrupaciones que sean:
+
+% Demagógica, es decir, si solo hizo gestiones individuales.
+
+esDelTipo(Agrupacion, demagogica) :-
+    realizoAccion(Agrupacion,_), % la agrupacion realiza una accion
+    forall(realizoAccion(Agrupacion,Accion), Accion == gestionIndividual(_,_,_)). % para todas las acciones realizadas por la agrupacion, que las acciones sean solo gestiones individuales
+
+% Burócrata: si no participó en ninguna lucha.
+
+esDelTipo(Agrupacion, burocrata) :-
+    realizoAccion(Agrupacion,_),    % la agrupacion realiza una accion
+    not(realizoAccion(Agrupacion,lucha(_))).
+
+% Transparente: si todas las acciones que realizó fueron genuinas.
+% Obra: es genuina si se hizo en un año en que no hubo elecciones.
+% Gestion individual: es genuina si la persona beneficiada era estudiante regular en el año en que se hizo.
+% Lucha: siempre es genuina.
+
+esDelTipo(Agrupacion, transparente) :-
+    realizoAccion(Agrupacion,_), % la agrupacion realiza una accion
+    forall(realizoAccion(Agrupacion,Accion), esGenuina(Accion)). % para todas las acciones realizadas por la agrupacion, las acciones son genuinas
+
+esGenuina(obra(Anio)) :-
+    realizoAccion(_, obra(Anio)),
+    not(elecciones(Anio)).
+
+esGenuina(gestionIndividual(_, Persona, Anio)) :-
+    %realizoAccion(_,gestionIndividual(_, Persona, Anio)),
+    estudiantes(_, Anio, Persona).
+
+esGenuina(lucha(_)).
+
+% Ver si hay alguna que tenga simultáneamente más de una de estas características.
+
+multiplesCaracteristicas(Agrupacion) :-
+    esDelTipo(Agrupacion,Caracterista),
+    esDelTipo(Agrupacion,OtraCaracteristica),
+    Caracterista \= OtraCaracteristica.
